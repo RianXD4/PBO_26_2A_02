467b4a86a2817b1ed872d4f3839e38bca5008e83
RianXD4 <adriansanda003@gmail.com>
2026-09-11 11:20:26 +0700

    Jobsheet03_Tugas06

--- Pertemuan 03/Tugas 01/TestLogistik.java
+++ Pertemuan 03/Tugas 01/TestLogistik.java
@@ -1,27 +1,40 @@
+import java.util.Scanner;
+
 public class TestLogistik {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int muatan;
         Kontainer kontainerAlfa = new Kontainer("REQ-9988", "PT. Maju Bersama", 5000);
         System.out.println("Nama Pemilik Konteiner: "+kontainerAlfa.getNamaPemilik());
         System.out.println("Kapasitas Maksimal: "+kontainerAlfa.getKapasitasMakasimal()+" kg");
-        
-        System.out.println("\nMemasukkan muatan baru seberat 6.000kg");
-        kontainerAlfa.tambahMuatan(6000);
-        System.out.println("Berat muatan saat ini: "+kontainerAlfa.getMuatanSaatIni()+" kg");
-
-        System.out.println("\nMemasukkan muatan baru seberat 4.000kg");
-        kontainerAlfa.tambahMuatan(4000);
-        System.out.println("Berat muatan saat ini: "+kontainerAlfa.getMuatanSaatIni()+" kg");
-
-        System.out.println("\nMenurunkan muatan seberat 500kg");
-        kontainerAlfa.turunkanMuatan(500);
-        System.out.println("Berat muatan saat ini: "+kontainerAlfa.getMuatanSaatIni()+" kg");
-
-        System.out.println("\nMenurunkan muatan seberat 1.500kg");
-        kontainerAlfa.turunkanMuatan(1500);
-        System.out.println("Berat muatan saat ini: "+kontainerAlfa.getMuatanSaatIni()+" kg");
-
-        System.out.println("\nMenurunkan muatan seberat 1.500kg");
-        kontainerAlfa.turunkanMuatan(1500);
-        System.out.println("Berat muatan saat ini: "+kontainerAlfa.getMuatanSaatIni()+" kg");
+        int pil = 0;
+         do {
+            System.out.println("1. Naik Muatan");
+            System.out.println("2. Turunkan Muatan");
+            System.out.println("0. Exit");
+            System.out.print("Pilihan: ");
+            pil = scan.nextInt();
+            switch (pil) {
+                case 1:
+                    System.out.print("Masukkan Muatan yang ingin ditambah: ");
+                    muatan = scan.nextInt();
+                    kontainerAlfa.tambahMuatan(muatan);
+                    System.out.println("Muatan Saat Ini: "+kontainerAlfa.getMuatanSaatIni());
+                    break;
+                case 2:
+                    System.out.print("Masukkan Muatan yang ingin diturunkan: ");
+                    muatan = scan.nextInt();
+                    kontainerAlfa.turunkanMuatan(muatan);
+                    System.out.println("Muatan Saat Ini: "+kontainerAlfa.getMuatanSaatIni());
+                    break;
+                case 0:
+                    
+                    break;
+                default:
+                    System.out.println("\nPilihan tidak valid coba lagi\n");
+                    break;
+            }
+        }while (pil != 0);
+        scan.close();
     }
 }
